7dcd8840fbb712ac6d33b4e0368267225f167865
monazoabi1 <munazoabi158@gmail.com>
2024-10-18 09:48:47 +0300

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,188 @@
+package pepse;
+
+/**
+ * The Constants class contains static fields representing various constants used
+ * throughout the application.
+ */
+public class Constants {
+
+    /**
+     * Represents the value 30.
+     */
+    public static final int THIRTY = 30;
+
+    /**
+     * Represents the value 20.
+     */
+    public static final int TWENTY = 20;
+
+    /**
+     * Represents the value 0.
+     */
+    public static final int ZERO = 0;
+
+    /**
+     * Represents the value 100.
+     */
+    public static final int ONE_HUNDRED = 100;
+
+    /**
+     * Represents the value 5.
+     */
+    public final static int FIVE = 5;
+
+    /**
+     * Represents the value 2.
+     */
+    public final static int TWO = 2;
+
+    /**
+     * Represents the value 90.
+     */
+    public final static int NINETY = 90;
+
+    /**
+     * Represents the value 120.
+     */
+    public final static int ONE_HUNDRED_TWENTY = 120;
+
+    /**
+     * Represents the value 20.
+     */
+    public final static int FRUIT_SIZE = 20;
+
+    /**
+     * Represents the value 50.
+     */
+    public final static int FIFTY = 50;
+
+    /**
+     * Represents the value 3.
+     */
+    public final static int THREE = 3;
+
+    /**
+     * Represents the value 10.
+     */
+    public final static int TEN = 10;
+
+    /**
+     * Represents the value 40.
+     */
+    public final static int FOURTY = 40;
+
+    /**
+     * Represents the value 200.
+     */
+    public final static int TWO_HUNDRED = 200;
+
+    /**
+     * Represents the value 800.
+     */
+    public final static int EIGHT_HUNDRED = 800;
+
+    /**
+     * Represents the value 0.5.
+     */
+    public final static double HALF = 0.5;
+
+    /**
+     * Represents the value 0.3.
+     */
+    public final static double TIME_TO_CHANGE_ANIMATION = 0.3;
+
+    /**
+     * Represents the Red component of the color (RGB: 212, 123, 74).
+     */
+    public final static int GROUND_COLOR_R = 212;
+
+    /**
+     * Represents the Green component of the color (RGB: 212, 123, 74).
+     */
+    public final static int GROUND_COLOR_G = 123;
+
+    /**
+     * Represents the Blue component of the color (RGB: 212, 123, 74).
+     */
+    public final static int GROUND_COLOR_B = 74;
+
+    /**
+     * Represents the value 7.
+     */
+    public final static int SEVEN = 7;
+
+    /**
+     * Represents the value 80.
+     */
+    public final static int EIGHTY = 80;
+
+    /**
+     * Represents the file path "assets/jump_0.png".
+     */
+    public final static String JUMP0 = "assets/jump_0.png";
+
+    /**
+     * Represents the file path "assets/jump_1.png".
+     */
+    public final static String JUMP1 = "assets/jump_1.png";
+
+    /**
+     * Represents the file path "assets/jump_2.png".
+     */
+    public final static String JUMP2 = "assets/jump_2.png";
+
+    /**
+     * Represents the file path "assets/jump_3.png".
+     */
+    public final static String JUMP3 = "assets/jump_3.png";
+
+    /**
+     * Represents the file path "assets/idle_0.png".
+     */
+    public final static String IDLE0 = "assets/idle_0.png";
+
+    /**
+     * Represents the file path "assets/idle_1.png".
+     */
+    public final static String IDLE1 = "assets/idle_1.png";
+
+    /**
+     * Represents the file path "assets/idle_2.png".
+     */
+    public final static String IDLE2 = "assets/idle_2.png";
+
+    /**
+     * Represents the file path "assets/idle_3.png".
+     */
+    public final static String IDLE3 = "assets/idle_3.png";
+
+    /**
+     * Represents the file path "assets/run_0.png".
+     */
+    public final static String RUN0 = "assets/run_0.png";
+
+    /**
+     * Represents the file path "assets/run_1.png".
+     */
+    public final static String RUN1 = "assets/run_1.png";
+
+    /**
+     * Represents the file path "assets/run_2.png".
+     */
+    public final static String RUN2 = "assets/run_2.png";
+
+    /**
+     * Represents the file path "assets/run_3.png".
+     */
+    public final static String RUN3 = "assets/run_3.png";
+
+    /**
+     * Represents the file path "assets/run_4.png".
+     */
+    public final static String RUN4 = "assets/run_4.png";
+
+    /**
+     * Represents the file path "assets/run_5.png".
+     */
+    public final static String RUN5 = "assets/run_5.png";
+}
